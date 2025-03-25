615a2397fe7d1d1da70d39fd0660e1f0af1783ac
Nasir Khan <37503821+mnkkhan77@users.noreply.github.com>
2025-03-25 22:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let curUrl = window.location.href;
+let curlen = curUrl.length;
+
+alert(`The length of the URL is: ${curlen}`);
